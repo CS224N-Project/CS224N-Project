f530fb8d142dd726fc67a41d8b8d88e2db4d5b34
Henry Neeb <hneeb@stanford.edu>
2017-03-08 14:05:33 -0800

    implemented evaluation and fit

--- main_v2.py
+++ main_v2.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 from preprocess import readOurData
 from model import Model
+import time
 
 # from util import print_sentence, write_conll, read_conll
 from data_util import load_and_preprocess_data
@@ -14,9 +15,9 @@ Set up classes and functions
 class Config(object):
 
     drop_out = 0.5
-    hidden_size = 20
+    hidden_size = 200
     batch_size = 32
-    epochs = 10
+    epochs = 2
     lr = 0.001
     l2Reg = 1.0e-6
     # built when we construct the model
@@ -131,7 +132,7 @@ class RNNModel(Model):
 
         # Add labels if not none
         if labels_batch is not None:
-            feed_dict[self.labels_placeholder] = labels_batch
+            feed_dict[self.labelsPH] = labels_batch
 
         return feed_dict
 
@@ -158,9 +159,10 @@ class RNNModel(Model):
         n_class = self.config.n_class
         batch_size = self.config.batch_size
         max_sentence = self.config.max_sentence
+        embedding_size = self.config.embedding_size
 
         # Define internal RNN Cells
-        cell1 = RNNCell(hidden_size, hidden_size, "cell1")
+        cell1 = RNNCell(embedding_size, hidden_size, "cell1")
         cell2 = RNNCell(hidden_size, hidden_size, "cell2")
 
         # Define our prediciton layer variables
@@ -204,51 +206,95 @@ class RNNModel(Model):
 
     def add_loss_op(self, pred):
         # Compute L2 loss
-        loss = tf.nn.l2_loss(self.labelsPH - pred)
-        loss = tf.reduce_mean(loss)
+        L2loss = tf.nn.l2_loss(self.labelsPH - pred)
+        L2loss = tf.reduce_mean(L2loss)
 
         # Apply L2 regularization
         reg_by_var = [tf.nn.l2_loss(v) for v in tf.trainable_variables()]
         regularization = tf.reduce_sum(reg_by_var)
 
-        totalCost = (10.0 * loss) + (self.l2RegPH * regularization)
-        return totalCost
+        loss = (10.0 * L2loss) + (self.l2RegPH * regularization)
+        return loss
 
-    def add_training_op(self, totalCost):
+    def add_training_op(self, loss):
         opt = tf.train.AdamOptimizer(learning_rate = self.config.lr)
-        train_op = opt.minimize(totalCost)
+        train_op = opt.minimize(loss)
         return train_op
 
     ## TODO: Add def evaluate(test_set)
+    def evaluate(self, pred):
+        diff = self.labelsPH - pred
+        prod = tf.matmul(diff, diff, transpose_a=True)
+        se = tf.reduce_sum(prod)
+        return se
+
+    def evaluate_on_batch(self, sess, inputs_batch, labels_batch, mask_batch):
+        feed = self.create_feed_dict(inputs_batch = inputs_batch,
+                                     mask_batch = mask_batch,
+                                     labels_batch=labels_batch,
+                                     dropout=self.config.drop_out,
+                                     l2_reg=self.config.l2Reg)
+        se = sess.run(self.eval, feed_dict=feed)
+        return se
 
     ### NO NEED TO UPDATE BELOW 
-    def train_on_batch(self, sess, inputs_batch, labels_batch):
-        feed = self.create_feed_dict(inputs_batch, labels_batch=labels_batch,
-                                     dropout=self.config.dropout)
+    def train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch):
+        feed = self.create_feed_dict(inputs_batch = inputs_batch,
+                                     mask_batch = mask_batch,
+                                     labels_batch=labels_batch,
+                                     dropout=self.config.drop_out,
+                                     l2_reg=self.config.l2Reg)
         _, loss = sess.run([self.train_op, self.loss], feed_dict=feed)
         return loss
 
-    def run_epoch(self, sess, parser, train_examples, dev_set):
-        prog = Progbar(target=1 + len(train_examples) / self.config.batch_size)
-        for i, (train_x, train_y) in enumerate(minibatches(train_examples, self.config.batch_size)):
-            loss = self.train_on_batch(sess, train_x, train_y)
+    def run_epoch(self, sess):
+        train_se = 0.0
+        prog = Progbar(target=1 + self.train_x.shape[0] / self.config.batch_size)
+        for i, (train_x, train_y, mask) in enumerate(minibatches(self.train_x, self.train_y, self.train_mask, self.config.batch_size)):
+            loss = self.train_on_batch(sess, train_x, train_y, mask)
+            train_se += self.evaluate_on_batch(sess, train_x, train_y, mask)
             prog.update(i + 1, [("train loss", loss)])
 
-        print "Evaluating on dev set",
-        dev_UAS, _ = parser.parse(dev_set)
-        print "- dev UAS: {:.2f}".format(dev_UAS * 100.0)
-        return dev_UAS
+        train_obs = self.train_x.shape[0]
+        train_mse = train_se / train_obs
 
-    def fit(self, sess, saver, parser, train_examples, dev_set):
-        best_dev_UAS = 0
-        for epoch in range(self.config.n_epochs):
-            print "Epoch {:} out of {:}".format(epoch + 1, self.config.n_epochs)
-            dev_UAS = self.run_epoch(sess, parser, train_examples, dev_set)
-            if dev_UAS > best_dev_UAS:
-                best_dev_UAS = dev_UAS
+        print 'Training MSE is {0}'.format(train_mse)
+
+        exit()
+
+        print "Evaluating on dev set",
+        dev_se = 0.0
+        for i, (dev_x, dev_y, dev_mask) in enumerate(minibatches(self.dev_x, self.dev_y, self.dev_mask, self.config.batch_size)):
+            dev_se += self.evaluate_on_batch(sess, dev_x, dev_y, dev_mask)
+
+        dev_obs = self.dev_x.shape[0]
+        dev_mse = dev_se / dev_obs
+
+        print "- dev MSE: {:.2f}".format(dev_mse)
+        return dev_mse
+
+    # def run_epoch(self, sess, parser, train_examples, dev_set):
+    #     prog = Progbar(target=1 + len(train_examples) / self.config.batch_size)
+    #     for i, (train_x, train_y) in enumerate(minibatches(train_examples, self.config.batch_size)):
+    #         loss = self.train_on_batch(sess, train_x, train_y)
+    #         prog.update(i + 1, [("train loss", loss)])
+    #
+    #     print "Evaluating on dev set",
+    #     dev_UAS, _ = parser.parse(dev_set)
+    #     print "- dev UAS: {:.2f}".format(dev_UAS * 100.0)
+    #     return dev_UAS
+
+    def fit(self, sess, saver):
+        best_dev_mse = 0
+        for epoch in range(self.config.epochs):
+            print "Epoch {:} out of {:}".format(epoch + 1, self.config.epochs)
+            dev_mse = self.run_epoch(sess)
+            if dev_mse < best_dev_mse:
+                best_dev_mse = dev_mse
                 if saver:
-                    print "New best dev UAS! Saving model in ./data/weights/parser.weights"
-                    saver.save(sess, './data/weights/parser.weights')
+                    print "New best dev MSE! Saving model in ./encoder.weights"
+                    # saver.save(sess, './encoder.weights', write_meta_graph = False)
+                    saver.save(sess, './encoder.weights')
             print
 
     ## add def eval here
@@ -269,6 +315,20 @@ class RNNModel(Model):
         self.config.n_class = train_y.shape[1]
         self.config.embedding_size = embeddingDictPad.shape[1]
         self.build()
+
+'''
+Evaluate model
+'''
+# def do_evaluate(model):
+#     config = model.config
+#     dev_x = model.dev_x
+#     dev_y = model.dev_y
+#     embedding = model.pretrained_embeddings
+#
+#     with tf.Graph().as_default():
+#         start = time.time()
+
+
 
 ''' 
 Creates Batch Data
@@ -302,25 +362,25 @@ embedding = '/Users/henryneeb/CS224N-Project/source/rcnn-master/beer/review+wiki
 # train_x_pad, train_y, train_mask, dev_x_pad, dev_y, dev_mask,embeddingDictPad = readOurData(train, dev, embedding)
 
 
-model = RNNModel(Config(), embedding, train, dev)
+# model = RNNModel(Config(), embedding, train, dev)
 
-exit()
+# exit()
 
 '''
 Get Embeddings
 '''
-embeddings = tf.constant(embeddingDictPad, dtype = tf.float32)
-embedInput = tf.nn.embedding_lookup(embeddings, inputPH)
-embedInput = tf.reshape(embedInput,
-                        shape = (batch_size, max_sentence, embedding_size))
+# embeddings = tf.constant(embeddingDictPad, dtype = tf.float32)
+# embedInput = tf.nn.embedding_lookup(embeddings, inputPH)
+# embedInput = tf.reshape(embedInput,
+#                         shape = (batch_size, max_sentence, embedding_size))
 # revEmbedInput = tf.reverse(embedInput, dims = [False, True, False])
 # embedInput = tf.unstack(embedInput, axis = 1)
-revEmbedInput = embedInput[::-1]
+# revEmbedInput = embedInput[::-1]
 
 '''
 Batch our Data
 '''
-iter = data_iterator(train_x_pad, train_y, batch_size, sentLen)
+# iter = data_iterator(train_x_pad, train_y, batch_size, sentLen)
 
 
 ##################
@@ -340,51 +400,58 @@ def main(debug=False):
     print 80 * "="
     config = Config()
     ## this is where we add our own data 
-    parser, embeddings, train_examples, dev_set, test_set = load_and_preprocess_data(debug)
-    if not os.path.exists('./data/weights/'):
-        os.makedirs('./data/weights/')
+    # parser, embeddings, train_examples, dev_set, test_set = load_and_preprocess_data(debug)
+    # if not os.path.exists('./data/weights/'):
+    #     os.makedirs('./data/weights/')
 
     with tf.Graph().as_default():
         print "Building model...",
         start = time.time()
         ## this is where we add our model class name 
         ## config is also a class name
-        model = RNNModel(config, embeddings)
-        rnn.model = model
+        model = RNNModel(config, embedding, train, dev)
+        # rnn.model = model
         print "took {:.2f} seconds\n".format(time.time() - start)
 
-        init = tf.initialize_all_variables()
+        init = tf.global_variables_initializer()
         # If you are using an old version of TensorFlow, you may have to use
         # this initializer instead.
         # init = tf.initialize_all_variables()
-        saver = None if debug else tf.train.Saver()
+        saver = tf.train.Saver()
 
         with tf.Session() as session:
-            rnn.session = session
             session.run(init)
 
             print 80 * "="
             print "TRAINING"
             print 80 * "="
-            ## this is a function is the model class 
-            model.fit(session, saver, parser, train_examples, dev_set)
-
-            if not debug:
-                print 80 * "="
-                print "TESTING"
-                print 80 * "="
-                print "Restoring the best model weights found on the dev set"
-                saver.restore(session, './data/weights/parser.weights')
-                print "Final evaluation on test set",
-                ## we won't have this. we need function in our model that will evaluate on test set
-                ## this is a function that will only calculate loss, "Evaluate function" takes inputs and compares to labels
-                ## ie model.evaluate(test_set)
-                loss = model.evaluate(test_set)
-                print "- test UAS: {:.2f}".format(UAS * 100.0)
-                print "Writing predictions"
-                with open('q2_test.predicted.pkl', 'w') as f:
-                    cPickle.dump(dependencies, f, -1)
-                print "Done!"
+            ## this is a function is the model class
+            # model.run_epoch(session)
+            model.fit(session, saver)
+            # model.train_on_batch(session, model.train_x[0:32,:], model.train_y[0:32,:], model.train_mask[0:32,:])
+            # train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch)
+            # model.fit(session, None)
+            # model.fit(session, saver)
+            #
+            # # train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch)
+            # # model.fit(session, saver, parser, train_examples, dev_set)
+            #
+            # if not debug:
+            #     print 80 * "="
+            #     print "TESTING"
+            #     print 80 * "="
+            #     print "Restoring the best model weights found on the dev set"
+            #     saver.restore(session, './parser.weights')
+            #     print "Final evaluation on test set",
+            #     ## we won't have this. we need function in our model that will evaluate on test set
+            #     ## this is a function that will only calculate loss, "Evaluate function" takes inputs and compares to labels
+            #     ## ie model.evaluate(test_set)
+            #     loss = model.evaluate(test_set)
+            #     print "- test UAS: {:.2f}".format(UAS * 100.0)
+            #     print "Writing predictions"
+            #     with open('q2_test.predicted.pkl', 'w') as f:
+            #         cPickle.dump(dependencies, f, -1)
+            #     print "Done!"
 
 if __name__ == '__main__':
     main()

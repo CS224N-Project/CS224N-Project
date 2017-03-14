359f946b32fa545c7b3e501a7a007eea78d7f787
Henry Neeb <hneeb@stanford.edu>
2017-03-14 00:08:08 -0700

    wrapped script in function

--- rationales_tensor.py
+++ rationales_tensor.py
@@ -13,19 +13,43 @@ def myio_read_rationales(path):
             data.append(item)
     return data
 
-rationale_data = myio_read_rationales('/Users/stanford/Desktop/Winter2017/CS224n/FinalProject/beer/annotations.json')
+def read_rationales_as_array(path):
+    # read in raw text
+    rationale_data = myio_read_rationales(path)
 
-max_len = 0
-for i in xrange(0,len(rationale_data)):
-	max_len = max(len(rationale_data[i]["x"]), max_len)
+    # find max data
+    max_len = 0
+    for i in xrange(0, len(rationale_data)):
+        max_len = max(len(rationale_data[i]["x"]), max_len)
 
-rationale_array = np.zeros((len(rationale_data), max_len), dtype = np.int32)
+    rationale_array = np.zeros((len(rationale_data), max_len), dtype=np.int32)
 
-for i in xrange(0,len(rationale_data)):
-	for j in xrange(0,len(rationale_data[i]["0"])):
-		begin_idx = rationale_data[i]["0"][j][0]
-		end_idx = rationale_data[i]["0"][j][1]
-		words = rationale_data[i]["x"]
-		rationale_array[i,begin_idx:end_idx] = 1
+    for i in xrange(0, len(rationale_data)):
+        for j in xrange(0, len(rationale_data[i]["0"])):
+            begin_idx = rationale_data[i]["0"][j][0]
+            end_idx = rationale_data[i]["0"][j][1]
+            words = rationale_data[i]["x"]
+            rationale_array[i, begin_idx:end_idx] = 1
 
-rationale_tensor = tf.Variable(rationale_array, name="rationale_tensor")
+    return rationale_array
+
+
+# read_rationales_as_array('/Users/henryneeb/CS224N-Project/source/rcnn-master/beer/annotations.json')
+#
+#
+# rationale_data = myio_read_rationales('/Users/henryneeb/CS224N-Project/source/rcnn-master/beer/annotations.json')
+#
+# max_len = 0
+# for i in xrange(0,len(rationale_data)):
+#     max_len = max(len(rationale_data[i]["x"]), max_len)
+#
+# rationale_array = np.zeros((len(rationale_data), max_len), dtype = np.int32)
+#
+# for i in xrange(0,len(rationale_data)):
+#     for j in xrange(0,len(rationale_data[i]["0"])):
+#         begin_idx = rationale_data[i]["0"][j][0]
+#         end_idx = rationale_data[i]["0"][j][1]
+#         words = rationale_data[i]["x"]
+#         rationale_array[i,begin_idx:end_idx] = 1
+#
+# rationale_tensor = tf.Variable(rationale_array, name="rationale_tensor")

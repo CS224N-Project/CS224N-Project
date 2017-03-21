755bba05a07ad25ae6e3d4451582ec9ca1ea4b74
Henry Neeb <hneeb@stanford.edu>
2017-03-20 21:27:21 -0700

    cleaned up and removed commented out code

--- generator-GRU.py
+++ generator-GRU.py
@@ -114,7 +114,6 @@ class GRUGeneratorModel(object):
         currBatch = tf.shape(x)[0]
         xDrop = tf.nn.dropout(x, self.dropoutPH)
         xRev = tf.reverse(xDrop, dims = [False, True, False])
-        # embeds = tf.concat(concat_dim=1, values = [xDrop, xRev])
 
         # Extract sizes
         hidden_size = self.config.hidden_size
@@ -182,36 +181,18 @@ class GRUGeneratorModel(object):
 
         # zLayer probabilities - each prob is prob of keeping word in review
         zProbs = tf.sigmoid(tf.matmul(finalStates, U) + c)
-        #zProbs = tf.select(self.maskPH,
-        #                   zProbs,
-        #                   tf.zeros(shape = tf.shape(zProbs), dtype = tf.float32))
-        # zProbs = tf.stop_gradient(zProbs)
-
-        # sample zprobs to pick which review words to keep. mask unselected words
-        # uniform = tf.random_uniform(shape = tf.shape(zProbs), minval=0, maxval=1) < zProbs
-        # uniform = tf.stop_gradient(
-        #     tf.random_uniform(shape=tf.shape(zProbs), minval=0,
-        #                       maxval=1) < zProbs, 'uniform')
-        #self.zPreds = tf.select(uniform,
-        #                        tf.ones(shape = tf.shape(uniform), dtype = tf.float32),
-        #                        tf.zeros(shape = tf.shape(uniform), dtype = tf.float32))
         
         self.zPreds = 1.0 / (1.0 + tf.exp(-60.0*(zProbs-0.5))) # sigmoid to simulate rounding
 
         self.zPreds = tf.select(self.maskPH, self.zPreds, tf.zeros(shape=tf.shape(zProbs), dtype=tf.float32))
         masks = tf.zeros(shape = tf.shape(zProbs), dtype = tf.int32) + self.maskId
         maskedInputs = tf.select(tf.cast(self.zPreds, tf.bool), self.inputPH, masks)
-        #maskedInputs = tf.select(uniform, self.inputPH, masks)
-
-        # compute probability of observing subsection of words
-        # probObs = self.zPreds * zProbs + (1.0 - self.zPreds) * (1.0 - zProbs)
-        # probObs = tf.reduce_prod(probObs, axis = 1, keep_dims=True)
-        # self.probObs = probObs
-        #maskFloats = tf.cast(self.maskPH, tf.float32)
         crossEntropy = -1.0 * (((self.zPreds * tf.log(zProbs + 0.001)) + ((1 - self.zPreds) * tf.log(1 - zProbs + 0.001)))) # * maskFloats)
         self.crossEntropy = crossEntropy
 
-
+        ###########
+        # ENCODER #
+        ###########
 
         # Return masked embeddings to pass to encoder
         embedding_shape = (-1,
@@ -222,9 +203,6 @@ class GRUGeneratorModel(object):
                                                   maskedInputs)
         maskedEmbeddings = tf.cast(maskedEmbeddings, tf.float32)
         maskedEmbeddings = tf.reshape(maskedEmbeddings, shape=embedding_shape)
-
-        # Use encoder to make predictions
-        # encoderPreds = self.encoder.add_prediction_op2(maskedEmbeddings)
 
         # Define our prediciton layer variables
         W = tf.get_variable(name='W',
@@ -266,26 +244,15 @@ class GRUGeneratorModel(object):
 
         # Compute L2 loss
         logPz = self.crossEntropy
-        #logPz = tf.Print(logPz, data=[tf.shape(logPz)], message="logPz", first_n=1, summarize=None)
         logPzSum = tf.reduce_sum(logPz, axis=1)
-        #logPzSum = tf.Print(logPzSum, data=[tf.shape(logPzSum)], message="logPzSum", first_n=1, summarize=None)
         predDiff = tf.square(self.labelsPH - pred)
-        #predDiff = tf.Print(predDiff, data=[tf.shape(predDiff)], message="predDiff", first_n=1, summarize=None)
 
-        #Zsum = tf.reduce_sum(self.zPreds, axis=1)
+        # Coherance and sparsity regularization
         Zsum = tf.reduce_sum(logPz, axis=1)
-        #Zsum = tf.Print(Zsum, data=[tf.shape(Zsum)], message="Zsum", first_n=1, summarize=None)
-
-        #self.zPreds = tf.Print(self.zPreds, data=[tf.shape(self.zPreds)], message="self.zPreds", first_n=1, summarize=None)
-
-        #Zdiff = tf.reduce_sum(tf.abs(self.zPreds[:,1:] - self.zPreds[:,:-1]), axis=1)
         Zdiff = tf.reduce_sum(tf.abs(logPz[:,1:] - logPz[:,:-1]), axis=1)
-        #Zdiff = tf.Print(Zdiff, data=[tf.shape(Zdiff)], message="Zdiff", first_n=1, summarize=None)
 
         costVec = predDiff + Zsum * sparsity_factor + Zdiff * coherent_factor
-        #costVec = tf.Print(costVec, data=[tf.shape(costVec)], message="costVec", first_n=1, summarize=None)
         costLogPz = tf.reduce_mean(costVec * logPzSum)
-        #costLogPz = tf.Print(costLogPz, data=[tf.shape(costLogPz)], message="costLogPz", first_n=1, summarize=None)
 
         # regularization
         reg_by_var = [tf.nn.l2_loss(v) for v in tf.trainable_variables()]
@@ -293,36 +260,9 @@ class GRUGeneratorModel(object):
 
         loss = 10.0 * costLogPz + regularization * self.l2RegPH
 
-
-
-        # L2loss = tf.nn.l2_loss((self.labelsPH - pred))
-        # # L2loss = tf.nn.l2_loss((self.labelsPH - pred) * self.probObs)
-        # L2loss = tf.reduce_mean(L2loss)
-        #
-        # # Apply L2 regularization - all vars
-        # reg_by_var = [tf.nn.l2_loss(v) for v in tf.trainable_variables()]
-        # regularization = tf.reduce_sum(reg_by_var)
-        #
-        # # apply L2 regularization to number of predictions
-        # # regPreds = tf.reduce_sum(tf.nn.l2_loss(self.zPreds))
-        #
-        # # apply reg to sequence
-        # sparsity_factor = 0.0003
-        # coherent_ratio = 2.0
-        # coherent_factor = sparsity_factor * coherent_ratio
-        # Zsum = tf.reduce_sum(self.zPreds, axis=0)
-        # Zdiff = tf.reduce_sum(tf.abs(self.zPreds[1:] - self.zPreds[:-1]), axis=0)
-        # sparsity_cost = tf.reduce_mean(Zsum) * sparsity_factor + tf.reduce_mean(
-        #     Zdiff) * coherent_factor
-        #
-        # loss = (10.0 * L2loss) + (self.l2RegPH * regularization) + sparsity_cost
-
         return loss
 
     def add_training_op(self, loss):
-        # opt = tf.train.AdamOptimizer(learning_rate = self.config.lr)
-        # train_op = opt.minimize(loss)
-
         opt = tf.train.AdamOptimizer(learning_rate=self.config.lr)
         grads = opt.compute_gradients(loss)
         self.grad_print = [(grad, var) for grad, var in grads]
@@ -331,7 +271,6 @@ class GRUGeneratorModel(object):
         train_op = opt.apply_gradients(capped_grads)
         return train_op
 
-    ## TODO: Add def evaluate(test_set)
     def evaluate(self, pred):
         diff = self.labelsPH - pred
         prod = tf.matmul(diff, diff, transpose_a=True)
@@ -366,13 +305,6 @@ class GRUGeneratorModel(object):
         predCorrect, predTotal = sess.run([self.predCorrect, self.predTotal], feed_dict = feed)
         return predCorrect, predTotal
 
-    # def run_precision_on_batch(self, sess, inputs_batch, labels_batch, mask_batch, sentLen, rationals):
-    #     predRational = self.run_rationals(sess, inputs_batch, labels_batch, mask_batch, sentLen, rationals)
-    #     overlap = predRational * rationals
-    #     predCorrect = tf.reduce_sum(overlap)
-    #     predTotal = tf.reduce_sum(predRational)
-    #     return predCorrect, predTotal
-
     def run_test_batch(self, sess, inputs_batch, labels_batch, mask_batch, sentLen, rationals):
         predCorrect, predTotal = self.run_rationals(sess,
                                                     inputs_batch,
@@ -388,9 +320,6 @@ class GRUGeneratorModel(object):
 
         return se, predCorrect, predTotal
 
-    # def run_test(self):
-
-    ### NO NEED TO UPDATE BELOW
     def train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch, sentLen):
         feed = self.create_feed_dict(inputs_batch = inputs_batch,
                                      mask_batch = mask_batch,
@@ -535,35 +464,12 @@ class GRUGeneratorModel(object):
         rationalPad = np.zeros(shape = (ration.shape[0], rationalDiff),
                                dtype = np.int32)
         paddedRational = np.append(ration, rationalPad, axis = 1)
-        # assert(paddedRational.shape[1] == train_x_pad.shape[1], 'rationals not padded correctly')
-        # assert(ration.shape[0] == rationalPad.shape[0], 'rationals have different number after padding')
-        #quickFix = np.zeros(shape = (6, paddedRational.shape[1]), dtype = np.int32)
-        #paddedRational = np.append(paddedRational, quickFix, axis = 0)
         self.rationals = paddedRational
         self.test_x = test_x_pad
         self.test_y = test_y
         self.test_mask = test_mask
         self.test_sentLen = test_sentLen
         self.build()
-
-    # def __init__(self, encoder):
-    #     self.train_x = encoder.train_x
-    #     self.train_y = encoder.train_y
-    #     self.train_mask = encoder.train_mask
-    #     self.dev_x = encoder.dev_x
-    #     self.dev_y = encoder.dev_y
-    #     self.dev_mask = encoder.dev_mask
-    #     self.pretrained_embeddings = encoder.pretrained_embeddings
-    #     self.train_sentLen = encoder.train_sentLen
-    #     self.dev_sentLen = encoder.dev_sentLen
-    #     # Update our config with data parameters
-    #     self.config = encoder.config
-    #     self.config.max_sentence = encoder.config.max_sentence
-    #     self.config.n_class = encoder.config.n_class
-    #     self.config.embedding_size = encoder.config.embedding_size
-    #     self.encoder = encoder
-    #     self.maskId = encoder.maskId
-    #     self.build()
 
 '''
 Read in Data
@@ -587,65 +493,27 @@ def main(debug=False):
     print "INITIALIZING"
     print 80 * "="
     config = Config()
-    ## this is where we add our own data
-    # parser, embeddings, train_examples, dev_set, test_set = load_and_preprocess_data(debug)
-    # if not os.path.exists('./data/weights/'):
-    #     os.makedirs('./data/weights/')
 
     with tf.Graph().as_default():
         print "Building model...",
         start = time.time()
-        ## this is where we add our model class name
-        ## config is also a class name
+
         generatorModel = GRUGeneratorModel(config, embedding, train, dev, test, annotations)
 
-        # generatorModel = RNNGeneratorModel(encoderModel)
         print "took {:.2f} seconds\n".format(time.time() - start)
 
         init = tf.global_variables_initializer()
-        # If you are using an old version of TensorFlow, you may have to use
-        # this initializer instead.
-        # init = tf.initialize_all_variables()
+
         saver = tf.train.Saver()
 
         with tf.Session() as session:
             session.run(init)
 
-            # tvar = tf.trainable_variables()
-            # for v in tvar:
-            #     print v
-
             print 80 * "="
             print "TRAINING"
             print 80 * "="
-            ## this is a function is the model class
-            # model.run_epoch(session)
-            generatorModel.fit(session, saver)
-            # model.train_on_batch(session, model.train_x[0:32,:], model.train_y[0:32,:], model.train_mask[0:32,:])
-            # train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch)
-            # model.fit(session, None)
-            # model.fit(session, saver)
-            #
-            # # train_on_batch(self, sess, inputs_batch, labels_batch, mask_batch)
-            # # model.fit(session, saver, parser, train_examples, dev_set)
-            #
-            # if not debug:
-            #     print 80 * "="
-            #     print "TESTING"
-            #     print 80 * "="
-            #     print "Restoring the best model weights found on the dev set"
-            #     saver.restore(session, './parser.weights')
-            #     print "Final evaluation on test set",
-            #     ## we won't have this. we need function in our model that will evaluate on test set
-            #     ## this is a function that will only calculate loss, "Evaluate function" takes inputs and compares to labels
-            #     ## ie model.evaluate(test_set)
-            #     loss = model.evaluate(test_set)
-            #     print "- test UAS: {:.2f}".format(UAS * 100.0)
-            #     print "Writing predictions"
-            #     with open('q2_test.predicted.pkl', 'w') as f:
-            #         cPickle.dump(dependencies, f, -1)
-            #     print "Done!"
 
+            generatorModel.fit(session, saver)
 
 if __name__ == '__main__':
     main()
